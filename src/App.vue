a79b9a2c59a8a7b554d8be1d31f9ffecd19a8d70
Manuel Castro <35335889+kwgeniusz@users.noreply.github.com>
2025-04-06 11:32:16 -0400

    feat: archivos principales de la aplicación

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <FormView />
+</template>
+
+<script setup>
+import FormView from '@/views/FormView.vue';
+</script>
+
+<style>
+body {
+  margin: 0;
+  padding: 0;
+  background-color: #f5f5f5;
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+}
+
+#app {
+  padding: 1rem;
+}
+</style>
